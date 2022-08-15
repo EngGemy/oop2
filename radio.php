8bc69e0246d37c2462443916b664c9809bdb6190
engmgemy <engmgemy@gmail.com>
2022-08-15 21:29:50 +0200

    oop

---
+++ radio.php
@@ -0,0 +1,13 @@
+<?php
+namespace App;
+
+class radio extends Boolean{
+
+
+    public function render(): string
+    {
+        return <<<HTML
+<input type="radio"  name="{$this->name}">
+HTML;
+    }
+}
